ff267edf8d01cb936370fd1a1db7de36795053dc
engphineas <phineastheengineer@gmail.com>
2023-09-17 10:08:26 -0700

    Revert "_printflast"
    This reverts commit 679b5d3c3be73f1b55bc1c07940b296dc0c91b74.

--- _printf.c
+++ _printf.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdarg.h>
-#include <unistd.h>
 /**
  * _printf - The function produces output according to the format
  * @format: format of character string
@@ -8,19 +6,18 @@
  */
 int _printf(const char *format, ...)
 {
-	int print_count = 0;
+	int printed_char = 0;
 	va_list chars_args;
 
 	if (format == NULL)
 		return (-1);
 	va_start(chars_args, format);
-
 	while (*format)
 	{
 		if (*format != '%')
 		{
 			write(1, format, 1);
-			print_count++;
+			printed_char++;
 		}
 		else
 		{
@@ -30,44 +27,27 @@ int _printf(const char *format, ...)
 			if (*format == '%')
 			{
 				write(1, format, 1);
-				print_count++;
+				printed_char++;
 			}
 			else if (*format == 'c')
 			{
-				char s = va_arg(chars_args, int);
-
-				write(1, &s, 1);
-				print_count++;
+				char character = va_arg(chars_args, int);
+				write(1, &character, 1);
+				printed_char++;
 			}
 			else if (*format == 's')
 			{
-				char *z = va_arg(chars_args, char*);
-				int length_z = 0;
+				char *string = va_arg(chars_args, char*);
+				int length_string = 0;
 
-				if (z == NULL)
-					z = "(null)";
-				while (z[length_z] != '\0')
-					length_z++;
-				write(1, &z, length_z);
-				print_count += length_z;
-			}
-			else if (*format == 'i' || *format == 'd')
-			{
-				int fig = va_arg(chars_args, int);
-				int length_fig = 0;
-				char fig_str[12];
-
-				snprintf(fig_str, sizeof(fig_str), "%d", fig);
-				while (fig_str[length_fig] != '\0')
-				{
-					write(1, &fig_str[length_fig], 1);
-					print_count++;
-					length_fig++;
-				}
+				while (string[length_string] != '\0')
+					length_string++;
+				write(1, string, length_string);
+				printed_char += length_string;
 			}
 		}
 		format++;
 	}
 	va_end(chars_args);
-	return (print_count);
+	return (printed_char);
 }
