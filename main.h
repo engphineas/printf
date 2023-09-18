ce37710d4bb78635e76422f2bd9f58c9af701207
Maggieboa <achymag2020@gmail.com>
2023-09-18 14:13:33 +0300

    main.h

--- main.h
+++ main.h
@@ -4,5 +4,11 @@
 #include<stdio.h>
 #include<unistd.h>
 int _printf(const char *format, ...);
+int print_binary(va_list types, char buffer[],int flags, int width, int precision, int size);
+int print_int(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_char(va_list types, char buffer[], int flags, int width, int precision, int size);
+
+
 
 #endif
