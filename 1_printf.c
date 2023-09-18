fbc08f8e3e6c8edc245eefe71baacc2f2e61f626
Maggieboa <achymag2020@gmail.com>
2023-09-18 18:18:27 +0300

    printf.c

---
+++ 1_printf.c
@@ -0,0 +1,182 @@
+#include "main.h"
+/**
+*printing_char-function to print a character
+* @types:args list
+* @buffer:an array to handle print
+* @flags:placeholder varaible for flags calculated
+* @width:placeholder varaible for width
+* @precision:placeholder variable for precision
+* @size:placeholder varaible for size
+* Return:returns total number of characters printed
+**/
+int printing_char(va_list types, char buffer[], int flags,
+int width, int precision, int size) /*line broken cz was too long*/
+{
+char c = va_arg(types, int);
+
+return (write_char(c, buffer, flags, width, precision, size));
+}
+/**
+* printing_string - Prints a string
+* @types:args list
+* @buffer:an array to handle print
+* @flags:placeholder varaible for flags calculated
+* @width:placeholder varaible for width
+* @precision:placeholder variable for precision
+* @size:placeholder varaible for size
+* Return:returns total number of characters printed
+**/
+int printing_string(va_list types, char buffer[], int flags,
+int width, int precision, int size) /*line broken cz was too long*/
+{
+int length = 0, i;
+char *str = va_arg(types, char *);
+
+UNUSED(buffer);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(precision);
+UNUSED(size);
+if (str == NULL)
+{
+str = "(null)";
+if (precision >= 6)
+str = "      ";
+}
+
+while (str[length] != '\0')
+length++;
+
+if (precision >= 0 && precision < length)
+length = precision;
+
+if (width > length)
+{
+if (flags & F_MIN)
+{
+write(1, &str[0], length);
+for (i = width - length; i > 0; i--)
+write(1, " ", 1);
+return (width);
+}
+else
+{
+for (i = width - length; i > 0; i--)
+write(1, " ", 1);
+write(1, &str[0], length);
+return (width);
+}
+}
+
+return (write(1, str, length));
+}
+/**
+*printing_percent - function to print the % sign
+*@types:args list
+*@buffer:an array to handle print
+*@flags:placeholder varaible for flags calculated
+*@width:placeholder varaible for width
+*@precision:placeholder variable for precision
+*@size:placeholder varaible for size
+*Return:returns total number of characters printed
+**/
+int printing_percent(va_list types, char buffer[], int flags,
+int width, int precision, int size)/*line broken cz was too long*/
+{
+UNUSED(types);
+UNUSED(buffer);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(precision);
+UNUSED(size);
+return (write(1, "%%", 1));
+}
+
+/**
+*printing_int - a function to print integer
+*@types:args list
+*@buffer:an array to handle print
+*@flags:placeholder varaible for flags calculated
+*@width:placeholder varaible for width
+*@precision:placeholder variable for precision
+*@size:placeholder varaible for size
+*Return:returns total number of characters printed
+**/
+int printing_int(va_list types, char buffer[], int flags,
+int width, int precision, int size)/*line broken cz was too long*/
+{
+int i = BUFFER_SIZE - 2;
+int is_negative = 0;
+long int x = va_arg(types, long int);
+unsigned long int num;
+
+x = size_number(x, size);
+
+if (x == 0)
+buffer[i--] = '0';
+
+buffer[BUFFER_SIZE - 1] = '\0';
+num = (unsigned long int)x;
+
+if (x < 0)
+{
+num = (unsigned long int)((-1) * x);
+is_negative = 1;
+}
+
+while (num > 0)
+{
+buffer[i--] = (num % 10) + '0';
+num /= 10;
+}
+
+i++;
+
+return (write_number(is_negative, i, buffer, flags, width, precision, size));
+}
+
+/**
+*printing_binary - function to print binary
+*@types: args list
+*@buffer: an array to handle print
+*@flags:  placeholder varaible for flags calculated
+*@width: placeholder varaible for width
+*@precision: placeholder variable for precision
+*@size: placeholder varaible for size
+*Return: returns total number of characters printed
+**/
+int printing_binary(va_list types, char buffer[],
+int flags, int width, int precision, int size)/*line broken cz was too long*/
+{
+unsigned int x, y, i, sum;
+unsigned int a[32];
+int count;
+
+UNUSED(buffer);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(precision);
+UNUSED(size);
+
+x = va_arg(types, unsigned int);
+y = 2147483647;/** unsigned int is a 32 bit datatum hence (2 ^ 31) - 1*/
+a[0] = x / y;
+for (i = 1; i < 32; i++)
+{
+y /= 2;
+a[i] = (x / y) % 2;
+}
+for (i = 0, sum = 0, count = 0; i < 32; i++)
+{
+sum += a[i];
+if (sum || i == 31)
+{
+char z = '0' + a[i];
+
+write(1, &z, 1);
+count++;
+}
+}
+return (count);
+}
+
