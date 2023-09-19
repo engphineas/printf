84b37c6677e059d839bca151a580538f4b99a5c0
engphineas <phineastheengineer@gmail.com>
2023-09-19 13:48:08 -0700

    printf_review

--- _printf.c
+++ _printf.c
@@ -1,31 +1,53 @@
 #include "main.h"
-#include <unistd.h>
 /**
-*_printf - The function produces output according to the format
-* @format: format of character string
-* Return: returns printed characters or -1 for error
-*/
+ * _printf - The function produces output according to the format
+ * @format: format of character string
+ * Return: returns printed characters or -1 for error
+ */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int length = 0;
+	int printed_char = 0;
+	va_list chars_args;
 
 	if (format == NULL)
 		return (-1);
+	va_start(chars_args, format);
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			printed_char++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				printed_char++;
+			}
+			else if (*format == 'c')
+			{
+				char character = va_arg(chars_args, int);
+				write(1, &character, 1);
+				printed_char++;
+			}
+			else if (*format == 's')
+			{
+				char *string = va_arg(chars_args, char*);
+				int length_string = 0;
 
-	va_start(args, format);
-
-	length = printf_format_specifier(format, args);
-	va_end(args);
-	return (length);
-}
-
-/**
-*write_handler-function to return a character
-*@c:placeholder variable for a character
-*Return: returns the printed character
-**/
-int write_handler(char c)
-{
-	return(write(1, &c, 1));
+				while (string[length_string] != '\0')
+					length_string++;
+				write(1, string, length_string);
+				printed_char += length_string;
+			}
+		}
+		format++;
+	}
+	va_end(chars_args);
+	return (printed_char);
 }
