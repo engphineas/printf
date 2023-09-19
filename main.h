e5f9666a6669f489eeaaec165c6cd34a54b31df6
Maggieboa <achymag2020@gmail.com>
2023-09-19 05:38:52 +0300

    main.h

--- main.h
+++ main.h
@@ -1,27 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-#define UNUSED(i) (void)(i)
-#define F_MIN 1
-#define BUFFER_SIZE 1024
-#define _LONG 2
-#define _SHORT 1
 #include<stdarg.h>
 #include<stdio.h>
 #include<unistd.h>
 int _printf(const char *format, ...);
-int printing_binary(va_list types, char buffer[], int flags,
-int width, int precision, int size);
-int printing_int(va_list types, char buffer[], int flags,
-int width, int precision, int size);
-int printing_string(va_list types, char buffer[], int flags,
-int width, int precision, int size);
-int printing_char(va_list types, char buffer[], int flags,
-int width, int precision, int size);
-
-int get_flags(const char *format, int *i);
-int get_width(const char *format, int *i, va_list list);
-int get_precision(const char *format, int *i, va_list list);
-int get_size(const char *format, int *i);
-
-long int size_number(long int num, int size);
 #endif
