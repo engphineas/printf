dc77a4ec039a303080c07a7ecb68740b31b575b6
engphineas <phineastheengineer@gmail.com>
2023-09-19 12:08:03 -0700

    printf1901

--- main.h
+++ main.h
@@ -3,6 +3,25 @@
 #include<stdarg.h>
 #include<stdio.h>
 #include<unistd.h>
+/**
+*fomart_specifiers-function struct
+*@_specifier:specifier
+*@function:function
+**/
+typedef struct fomart_specifiers
+{
+char *_specifier;
+int (*function)(va_list args);
+}fmt_spec;
+int write_handler(char c);
 int _printf(const char *format, ...);
-
+int printf(const char *format, ...);
+int _print_format(const char *format, va_list args);
+int printf_format_specifier(const char *format, va_list char_args);
+int specifier(char format, va_list args);
+int integer(va_list args);
+int string(va_list char_args);
+int character(va_list int_args);
+int check_specifier(char char_initial, char format, int length);
+int check_char(char specified_char);
 #endif
